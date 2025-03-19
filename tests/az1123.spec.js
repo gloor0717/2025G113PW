1a13aa7669af16531c6b28fd15cf4d9280fae814
gloor0717 <gian-luca@gloor.dev>
2025-03-19 17:16:14 +0100

    Az1123 Default Test

---
+++ tests/az1123.spec.js
@@ -0,0 +1,40 @@
+import { test, expect } from "@playwright/test";
+
+test('User Story Az 1123: Utilisation du module de calcul "Technologies and measures"', async ({
+  page,
+}) => {
+  await page.goto("https://coollifedev.hevs.ch/");
+
+  await page.getByRole("button", { name: "GO TO APP" }).click();
+
+  if (await page.isVisible("button:has-text('Accept all cookies')")) {
+    await page.getByRole("button", { name: "Accept all cookies" }).click();
+  }
+
+  const searchInput = page.locator("#place-input");
+  await searchInput.fill("Geneve");
+
+  await page.keyboard.press("Enter");
+
+  await page.waitForTimeout(2000);
+
+  await page.getByRole("button", { name: "Layers" }).click();
+  await page.getByLabel("LAU 2").check();
+
+  await page.mouse.click(500, 500);
+
+  await page.locator("#funct-test-cms").click();
+
+  await page.locator('button:has-text("CM - Technologies and measures")').click();
+
+  await page.locator("#funct-test-run-cm-button").click();
+
+  const resultPanel = page.locator("app-right-side-panel");
+  await expect(resultPanel).toBeVisible();
+
+  const valueElement = resultPanel.locator("td.value-column");
+  const valueText = await valueElement.textContent();
+  const value = parseFloat(valueText.trim());
+
+  expect(value).toBeGreaterThanOrEqual(0.1);
+});
